9bc36a0778f159e8f5b7cc2279b894aafb8567ad
Hrushikesh <hrushikesh.surkar@greatplaceitservices.com>
2024-04-11 22:04:10 +0530

    Initial commit of the project

---
+++ src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MainLayout.scss";
+
+const MainLayout = () => {
+  return <div>MainLayout</div>;
+};
+
+export default MainLayout;
---
+++ src/pages/Blogs/Blogs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Blogs.scss";
+
+const Blogs = () => {
+  return <div>Blogs</div>;
+};
+
+export default Blogs;
